fd8349902557e18b031544fbf73718d1142b64f6
Matteo Calabrò (Personal) <matticala@gmail.com>
2017-03-18 10:59:35 +0100

    Moved all private helpers into sub-object `Private` to have a cleaner code More helping functions `Nil`UUID as per RFC specification Removed SHA-224 and 384 Random over namespace function

--- src/main/scala/com/github/matticala/scala/util/UUID.scala
+++ src/main/scala/com/github/matticala/scala/util/UUID.scala
@@ -1,15 +1,14 @@
-package com.github
-package matticala.scala.util
+package com.github.matticala.scala
+package util
 
-import java.io._
 import java.nio.charset.StandardCharsets
 import java.security.{MessageDigest, SecureRandom}
 
 import scala.util.Try
 
-final class UUID (val msb: Long, val lsb: Long) extends Serializable with Equals with Comparable[UUID] {
+sealed class UUID (val msb: Long, val lsb: Long) extends Serializable with Equals with Comparable[UUID] {
 
-  import UUID.hex
+  import UUID.Private.hex
 
   val version: Int = ((msb >> 12) & 0x0f).toInt
 
@@ -19,12 +18,6 @@ final class UUID (val msb: Long, val lsb: Long) extends Serializable with Equals
     val msb = this.msb compareTo o.msb
     if (msb == 0) this.lsb compareTo o.lsb else msb
   }
-
-  def asJava: java.util.UUID = UUID.toJava(this)
-
-  def toBytes: Bytes = this.msb.toBytes ++ this.lsb.toBytes
-
-  def canEqual(other: Any): Boolean = other.isInstanceOf[UUID] || other.isInstanceOf[java.util.UUID]
 
   override def equals(that: Any): Boolean = that match {
     case that: UUID ⇒ (this eq that) || (this.msb == that.msb && this.lsb == that.lsb)
@@ -37,43 +30,40 @@ final class UUID (val msb: Long, val lsb: Long) extends Serializable with Equals
     (xor >> 32).toInt ^ xor.toInt
   }
 
+  def canEqual(other: Any): Boolean = other.isInstanceOf[UUID] || other.isInstanceOf[java.util.UUID]
+
   def copy(msb: Long = this.msb, lsb: Long = this.lsb) = new UUID(msb, lsb)
 
+  def toBytes: Bytes = this.msb.toBytes ++ this.lsb.toBytes
+
   def tupled: (Long, Long) = (msb, lsb)
+
+  implicit def asJava: java.util.UUID = new java.util.UUID(this.msb, this.lsb)
 }
 
 object UUID {
-  import java.lang.Long.BYTES
 
-  private lazy val generator: SecureRandom = new SecureRandom()
+  case object Nil extends UUID(0, 0)
 
-  final val namespaceAndMD5: String ⇒ String ⇒ UUID = namespaceUUID(MessageDigest.getInstance("MD5").digest)(3)
+  import Private._
 
-  final val namespaceAndSHA1: String ⇒ String ⇒ UUID = namespaceUUID(MessageDigest.getInstance("SHA").digest)(5)
+  val namespaceAndMD5: String ⇒ String ⇒ UUID = namespaceAndNameUUID(MessageDigest.getInstance("MD5").digest)(3)
 
-  final val namespaceAndSHA224: String ⇒ String ⇒ UUID = namespaceUUID(MessageDigest.getInstance("SHA-224").digest)(5)
+  val namespaceAndSHA1: String ⇒ String ⇒ UUID = namespaceAndNameUUID(MessageDigest.getInstance("SHA").digest)(5)
 
-  final val namespaceAndSHA256: String ⇒ String ⇒ UUID = namespaceUUID(MessageDigest.getInstance("SHA-256").digest)(5)
+  val namespaceAndSHA256: String ⇒ String ⇒ UUID = namespaceAndNameUUID(MessageDigest.getInstance("SHA-256").digest)(8)
 
-  final val namespaceAndSHA384: String ⇒ String ⇒ UUID = namespaceUUID(MessageDigest.getInstance("SHA-384").digest)(5)
+  val namespaceAndSHA512: String ⇒ String ⇒ UUID = namespaceAndNameUUID(MessageDigest.getInstance("SHA-512").digest)(9)
 
-  final val namespaceAndSHA512: String ⇒ String ⇒ UUID = namespaceUUID(MessageDigest.getInstance("SHA-512").digest)(5)
+  val randomOnNamespace: String ⇒ UUID = namespaceUUID(MessageDigest.getInstance("SHA-256").digest)(8)(_)()
 
-  final val version4: UUID = random
+  val version4: UUID = random
 
-  final def version3(namespace: String, name: String): UUID = namespaceAndMD5(namespace)(name)
+  def version3(namespace: String, name: String): UUID = namespaceAndMD5(namespace)(name)
 
-  final def version5(namespace: String, name: String): UUID = namespaceAndSHA1(namespace)(name)
+  def version5(namespace: String, name: String): UUID = namespaceAndSHA1(namespace)(name)
 
-  def unapply(arg: UUID): Option[(String)] = Some(arg.toString)
-
-  def unapply(arg: String): Option[UUID] = UUID.fromString(arg).toOption
-
-  def random: UUID = {
-    val array: Bytes = Array.ofDim(2*BYTES)
-    generator.nextBytes(array)
-    UUID.fromBytes(setUUIDBytes(array, 4))
-  }
+  def random: UUID = fromBytes(setUUIDBytes(getRandomBytes, 4))
 
   def fromString(string: String): Try[UUID] = Try {
     val parts: Array[String] = string.split("-")
@@ -85,33 +75,46 @@ object UUID {
     )
   }
 
-  implicit def toJava(uuid: UUID): java.util.UUID = new java.util.UUID(uuid.msb, uuid.lsb)
+  def unapply(arg: UUID): Option[(String)] = Some(arg.toString)
+
+  def unapply(arg: String): Option[UUID] = UUID.fromString(arg).toOption
 
   implicit def fromJava(uuid: java.util.UUID): UUID = new UUID(uuid.getMostSignificantBits, uuid.getLeastSignificantBits)
 
-  private def fromBytes(array: Bytes): UUID = {
-    require(array.length > 15, "Data must be 16+ bytes long")
-    val (msb, lsb) = array.take(2*BYTES).splitAt(BYTES)
-    new UUID(bytes2long(msb), bytes2long(lsb))
-  }
+  private[UUID] object Private {
 
-  private def namespaceUUID(digest: Bytes ⇒ Bytes)(version: Byte)(namespace: String)(name: String): UUID = {
-    val array = digest(namespace.getBytes(StandardCharsets.UTF_8) ++ name.getBytes(StandardCharsets.UTF_8))
-    UUID.fromBytes(setUUIDBytes(array, version))
-  }
+    import java.lang.Long.BYTES
 
-  private def setUUIDBytes(source: Bytes, version: Int): Bytes = {
-    val bytes = new Bytes(source.length)
-    Array.copy(source, 0, bytes, 0, source.length)
-    bytes(6) = ((bytes(6) & 0x0f) | (version << 4)).toByte
-    bytes(8) = ((bytes(8) & 0x3f) | 0x80).toByte
-    bytes
-  }
+    private lazy val generator: SecureRandom = new SecureRandom()
 
-  private def bytes2long(bytes: Bytes): Long = bytes.take(BYTES).foldLeft(0L)((acc, byte) ⇒ (acc << BYTES) | (byte & 0xff))
+    def getRandomBytes: Bytes = generator.nextBytes(2*BYTES)
 
-  private def hex(value: Long, digits: Int): String = {
-    val hi: Long = 1L << (digits * 4)
-    (hi | (value & (hi - 1))).toHexString.substring(1)
+    def fromBytes(array: Bytes): UUID = {
+      val (msb, lsb) = array.take(2*BYTES).splitAt(BYTES)
+      new UUID(bytes2long(msb), bytes2long(lsb))
+    }
+
+    def namespaceAndNameUUID(digest: Bytes ⇒ Bytes)(version: Byte)(namespace: String)(name: String): UUID =
+      namespaceUUID(digest)(version)(namespace)(name.getBytes(StandardCharsets.UTF_8))
+
+    def namespaceUUID(digest: Bytes ⇒ Bytes)(version: Byte)(namespace: String)(addition: Bytes = getRandomBytes): UUID = fromBytes {
+      val array = digest(namespace.getBytes(StandardCharsets.UTF_8) ++ addition)
+      setUUIDBytes(array, version)
+    }
+
+    def setUUIDBytes(source: Bytes, version: Int): Bytes = {
+      val bytes = new Bytes(source.length)
+      Array.copy(source, 0, bytes, 0, source.length)
+      bytes(6) = ((bytes(6) & 0x0f) | (version << 4)).toByte
+      bytes(8) = ((bytes(8) & 0x3f) | 0x80).toByte
+      bytes
+    }
+
+    def bytes2long(bytes: Bytes): Long = bytes.take(BYTES).foldLeft(0L)((acc, byte) ⇒ (acc << BYTES) | (byte & 0xff))
+
+    def hex(value: Long, digits: Int): String = {
+      val hi: Long = 1L << (digits * 4)
+      (hi | (value & (hi - 1))).toHexString.substring(1)
+    }
   }
 }
--- src/main/scala/com/github/matticala/scala/util/package.scala
+++ src/main/scala/com/github/matticala/scala/util/package.scala
@@ -3,6 +3,7 @@ package matticala
 package scala
 
 import java.lang.Long.BYTES
+import java.util.Random
 
 package object util {
 
@@ -10,6 +11,16 @@ package object util {
 
   implicit class ExtLong(val underlying: Long) extends AnyVal {
     def toBytes: Bytes = Array.tabulate(BYTES)(i ⇒ ((underlying >> (BYTES - 1 - i) * BYTES) & 0xff).toByte)
+  }
+
+  implicit class ExtRandom(val underlying: Random) extends AnyVal {
+
+    def nextBytes(size: Int): Bytes = {
+      val bytes: Bytes = Array.ofDim(size)
+      underlying.nextBytes(bytes)
+      bytes
+    }
+
   }
 
 }
